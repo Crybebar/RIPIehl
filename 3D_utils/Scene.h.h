6d3fb145a204a2103f9b118bbbf133d432c54a19
Crybebar <jerome.marthe2342@gmail.fr>
2015-12-15 11:06:45 +0100

    bon, ca va marcher la ><

---
+++ 3D_utils/Scene.h.h
@@ -0,0 +1,28 @@
+//
+// Created by mckoala on 15/12/15.
+//
+
+#ifndef RIPIEHL_SCENE_H_H
+#define RIPIEHL_SCENE_H_H
+
+#include <GL/gl.h>
+
+void drawOrigin(){
+    glBegin(GL_LINE);
+
+    glColor3f(1.f, 0.f, 0.,f);
+    glVertex3f(0.f, 0.f, 0.f);
+    glVertex3f(1.f, 0.f, 0.f);
+
+    glColor3f(0.f, 1.f, 0.,f);
+    glVertex3f(0.f, 0.f, 0.f);
+    glVertex3f(0.f, 1.f, 0.f);
+
+    glColor3f(0.f, 0.f, 1.,f);
+    glVertex3f(0.f, 0.f, 0.f);
+    glVertex3f(0.f, 0.f, 1.f);
+
+    glEnd();
+}
+
+#endif //RIPIEHL_SCENE_H_H
