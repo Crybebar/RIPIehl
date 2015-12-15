16a17cf35b7c102187483466dda4fbab4fc054c3
Crybebar <jerome.marthe2342@gmail.fr>
2015-12-15 12:09:46 +0100

    debut de camera mobile

--- 3D_utils/Scene.h
+++ 3D_utils/Scene.h
@@ -2,27 +2,28 @@
 // Created by mckoala on 15/12/15.
 //
 
-#ifndef RIPIEHL_SCENE_H_H
-#define RIPIEHL_SCENE_H_H
+#ifndef RIPIEHL_SCENE_H
+#define RIPIEHL_SCENE_H
 
 #include <GL/gl.h>
 
 void drawOrigin(){
-    glBegin(GL_LINE);
 
-    glColor3f(1.f, 0.f, 0.,f);
+    glBegin(GL_LINES);
+
+    glColor3f(1.f, 0.f, 0.f);
     glVertex3f(0.f, 0.f, 0.f);
     glVertex3f(1.f, 0.f, 0.f);
 
-    glColor3f(0.f, 1.f, 0.,f);
+    glColor3f(0.f, 1.f, 0.f);
     glVertex3f(0.f, 0.f, 0.f);
     glVertex3f(0.f, 1.f, 0.f);
 
-    glColor3f(0.f, 0.f, 1.,f);
+    glColor3f(0.f, 0.f, 1.f);
     glVertex3f(0.f, 0.f, 0.f);
     glVertex3f(0.f, 0.f, 1.f);
 
     glEnd();
 }
 
-#endif //RIPIEHL_SCENE_H_H
+#endif //RIPIEHL_SCENE_H
--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 
 #include "Math/Mat4.h"
+#include "3D_utils/Scene.h"
 
 using namespace std;
 
@@ -30,7 +31,7 @@ int main() {
     }
 
     // Création de la fenêtre
-    fenetre = SDL_CreateWindow("RIPIehl alpha", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+    fenetre = SDL_CreateWindow("RIPIehl alpha", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 
     if(fenetre == 0){
         std::cout << "Erreur lors de la creation de la fenetre : " << SDL_GetError() << std::endl;
@@ -55,23 +56,68 @@ int main() {
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); //1 pour actif
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24); //24 pour la precisiona 24 bits
 
+
+    glViewport(0 ,0 , 800, 800);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0, 0, -10);
+
+    glMatrixMode(GL_PROJECTION);
+    gluPerspective(30, 1, 0.1, 1000);
+
+
+
     while(!terminer){
         glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+
+        /*
         glBegin(GL_TRIANGLES);
         {
             glColor3f(1.0f, 1.0f, 1.0f);
-            glVertex2f(0.0f, 1.0f);
-            glVertex2f(-1.0f, -1.0f);
-            glVertex2f(1.0f, -1.0f);
+            glVertex3f(0.0f, 1.0f, 0.f);
+            glVertex3f(-1.0f, 0.f, 0.f);
+            glVertex3f(1.0f, 0.f, 0.f);
         }
         glEnd();
+        */
+
+        drawOrigin();
 
         glFlush();
 
-        SDL_WaitEvent(&evenements);
-        if(evenements.window.event == SDL_WINDOWEVENT_CLOSE) {
-            terminer = true;
+        //if any event catch
+        if( SDL_PollEvent(&evenements) == 1) {
+            if (evenements.window.event == SDL_WINDOWEVENT_CLOSE) {
+                terminer = true;
+            }
+
+            if (evenements.type == SDL_KEYDOWN){
+                switch(evenements.key.keysym.sym) {
+                    case SDLK_z:
+                        glMatrixMode(GL_MODELVIEW);
+                        glTranslatef(0, 0, 0.1f);
+                        break;
+
+                    case SDLK_s:
+                        glMatrixMode(GL_MODELVIEW);
+                        glTranslatef(0, 0, -0.1f);
+                        break;
+
+                    case SDLK_q:
+                        glMatrixMode(GL_MODELVIEW);
+                        glTranslatef(0.1f, 0, 0);
+                        break;
+
+                    case SDLK_d:
+                        glMatrixMode(GL_MODELVIEW);
+                        glTranslatef(-0.1f, 0, 0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
         }
 
         // Nettoyage de l'écran
